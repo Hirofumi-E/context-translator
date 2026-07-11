03d8d98ad32765195c43f63d80b479d636793c27
Hiro <konahi.sub@gmail.com>
2026-07-11 20:45:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "context-translator-v2";
+const CACHE_NAME = "context-translator-v3";
 const APP_ASSETS = [
   "./",
   "./index.html",
@@ -8,6 +8,7 @@ const APP_ASSETS = [
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
@@ -16,6 +17,7 @@ self.addEventListener("activate", event => {
       Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
